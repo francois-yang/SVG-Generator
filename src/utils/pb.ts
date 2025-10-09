804c400973410a434f07e76a5ad70d7e9955df7a
francois-yang <francois.yang@edu.univ-fcomte.fr>
2025-10-09 09:54:25 +0200

    essai 6

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -3,6 +3,6 @@ import type { TypedPocketBase } from "./pocketbase-types";
 var path='';
 if(import.meta.env.MODE === 'development')
     path = 'http://localhost:8090'    //localhost = machine de dev
-else path = 'http://localhost:80<xx>'   //localhost = machine de déploiement
+else path = 'http://localhost:8098'   //localhost = machine de déploiement
 const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
